3a71fd97bab57d3cfb0ff892d14657151229d239
anko9801 <daiusa3@icloud.com>
2021-08-21 10:55:36 +0900

    add index

--- sql/0_Schema.sql
+++ sql/0_Schema.sql
@@ -27,6 +27,8 @@ CREATE TABLE `isu_condition` (
   `message` VARCHAR(255) NOT NULL,
   `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6),
   INDEX timestamp_index (`timestamp`),
+  INDEX time_desc_index (`timestamp` DESC),
+  INDEX isu_index (`jia_isu_uuid`),
   INDEX isu_timestamp_desc_index (`jia_isu_uuid`, `timestamp` DESC),
   PRIMARY KEY(`id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
